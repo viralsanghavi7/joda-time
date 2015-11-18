606571aa10766b9c0edf27a9fd8e6cb8e5d1027d
Chetan <chetanpawar0989@gmail.com>
2015-11-18 18:59:39 -0500

    Testing post-commit and execution of aws_tasks script on aws

--- tempFiles/M3.sh
+++ tempFiles/M3.sh
@@ -5,11 +5,11 @@ var=$(node ~/DevOps/getIP/main.js)
 tar -zcvf ../../joda-time.tgz ../../joda-time/
 
 # copy joda-time library code
-scp -i ~/DevOps/viral/joda-time/tempFiles/milestone3.pem ~/DevOps/viral/joda-time.tgz ubuntu@$var:~
+scp -i ~/DevOps/viral/joda-time/tempFiles/devops3.pem ~/DevOps/viral/joda-time.tgz ubuntu@$var:~
 
 # copy redis-stable version code
-scp -i ~/DevOps/viral/joda-time/tempFiles/milestone3.pem ~/DevOps/viral/joda-time/tempFiles/redis-stable.tgz ubuntu@$var:~
+scp -i ~/DevOps/viral/joda-time/tempFiles/devops3.pem ~/DevOps/viral/joda-time/tempFiles/redis-stable.tgz ubuntu@$var:~
 
 # copy aws_tasks.sh which contains commands to run the server on aws instance
-scp -i ~/DevOps/viral/joda-time/tempFiles/milestone3.pem ~/DevOps/viral/joda-time/tempFiles/aws_tasks.sh ubuntu@$var:~
+scp -i ~/DevOps/viral/joda-time/tempFiles/devops3.pem ~/DevOps/viral/joda-time/tempFiles/aws_tasks.sh ubuntu@$var:~
 
