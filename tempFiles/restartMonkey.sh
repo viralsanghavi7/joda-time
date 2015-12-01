3287d2c961f33c02835181ca9edb8200f53d6d9f
Chetan <chetanpawar0989@gmail.com>
2015-12-01 11:08:06 -0500

    testing for restart monkey

--- tempFiles/restartMonkey.sh
+++ tempFiles/restartMonkey.sh
@@ -3,5 +3,14 @@
 # this playbook will restart the instances.
 ansible-playbook -i ec2.py restart.yml --private-key ~/Downloads/devops3.pem
 
+#echo "************************** waiting for servers ssh port to open up **************************"
+#sleep 5m
+
+./ec2.py --refresh-cache
+
+echo "************************** running site:9000 again **************************"
 # this command will start maven site on instances
 ansible -i ec2.py --private-key ~/Downloads/devops3.pem -u ubuntu us-east-1 -a /home/ubuntu/startSite.sh -m shell
+
+echo "************************** waiting for mvn site to get up **************************"
+sleep 5m
--- tempFiles/startSite.sh
+++ tempFiles/startSite.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+echo "***********************Running startSite.sh***********************"
 cd joda-time
 
 nohup mvn site:run </dev/null &>/dev/null &
