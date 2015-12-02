b0a18ac88904c1aac614356dc06929ad4f883ca1
Chetan <chetanpawar0989@gmail.com>
2015-12-02 18:52:08 -0500

    final commit from Chetan

--- tempFiles/restartMonkey.sh
+++ tempFiles/restartMonkey.sh
@@ -4,7 +4,7 @@
 ansible-playbook -i ec2.py restart.yml --private-key ~/Downloads/devops3.pem
 
 #echo "************************** waiting for servers ssh port to open up **************************"
-#sleep 5m
+sleep 5m
 
 ./ec2.py --refresh-cache
 
@@ -13,4 +13,7 @@ echo "************************** running site:9000 again ***********************
 ansible -i ec2.py --private-key ~/Downloads/devops3.pem -u ubuntu us-east-1 -a /home/ubuntu/startSite.sh -m shell
 
 echo "************************** waiting for mvn site to get up **************************"
-sleep 5m
+sleep 2m
+
+echo "************************** curl to instance dns to see if the site is up **************************"
+node ~/DevOps/getIP/curl1.js
